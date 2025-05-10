30b66dc1bc7bde7db3a1753a56cec23204644919
Aakarshan Mani Giri <153081239+AakarshanManiGiri@users.noreply.github.com>
2025-05-10 13:21:51 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-from ucimlrepo import fetch_ucirepo 
-abalone = fetch_ucirepo(id=1)  
-X = abalone.data.features 
-y = abalone.data.targets 
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import mean_absolute_error
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
+import plotly.express as px
+import plotly.graph_objs as go
